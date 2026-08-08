cd43999bdb447c69e4f009a9a4be028c5f3ddb19
mepranavpatil <mepranavpatil@gmail.com>
2026-08-08 12:40:43 +0530

    .

--- P1 Automation with python/app.py
+++ P1 Automation with python/app.py
@@ -1,12 +1,31 @@
 import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+# Load the original workbook
 workbook = xl.load_workbook('data/transactions.xlsx')
 worksheet = workbook['Sheet1']
-cell = worksheet['a1']
-cell=worksheet.cell(1,1)
-print (worksheet.max_column)
 
-
+# Update column D based on column C
 for row in range(2, worksheet.max_row + 1):
-    for col in range(1, worksheet.max_column + 1):
-        cell = worksheet.cell(row, col)
-        print(cell.value)
+    price_cell = worksheet.cell(row, 3)
+    corrected_price = price_cell.value * 0.9
+
+    corrected_price_cell = worksheet.cell(row, 4)
+    corrected_price_cell.value = corrected_price
+
+# Create chart using column D
+ref = Reference(
+    worksheet,
+    min_row=2,
+    max_row=worksheet.max_row,
+    min_col=4,
+    max_col=4
+)
+
+bar_chart = BarChart()
+bar_chart.add_data(ref)
+
+worksheet.add_chart(bar_chart, "E1")
+
+# Save as a NEW file
+workbook.save('data/transactions2.xlsx')
